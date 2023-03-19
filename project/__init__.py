9ce6a847d9218efed281e5956e14da3def5af631
Anees Asghar <aneesasgharsyed@gmail.com>
2023-03-19 00:21:05 +0000

    Remove unused imports; Fix code formatting

--- project/__init__.py
+++ project/__init__.py
@@ -1,39 +1,41 @@
 import os
 import secrets
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+
 db = SQLAlchemy()
+
 
 def create_app():
 
-	project_dir = os.path.dirname(os.path.abspath(__file__))
-	database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookish_db.db"))
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    database_file = "sqlite:///{}".format(
+        os.path.join(project_dir, "bookish_db.db"))
 
-	app = Flask(__name__)
-	app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_file
 
-	secret = secrets.token_urlsafe(32)
-	app.config['SECRET_KEY'] = secret
+    secret = secrets.token_urlsafe(32)
+    app.config['SECRET_KEY'] = secret
 
-	db.init_app(app)
+    db.init_app(app)
 
-	login_manager = LoginManager()
-	login_manager.login_view = 'auth.login'
-	login_manager.init_app(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
-	from .models import User, User_Book
+    from .models import User, User_Book
 
-	@login_manager.user_loader
-	def load_user(user_id):
-		return User.query.get(int(user_id))
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
-	from .main import main as main_blueprint
-	app.register_blueprint(main_blueprint)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-	from .auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
-	return app
+    return app
--- project/utilities.py
+++ project/utilities.py
@@ -1,5 +1,4 @@
 import requests
-
 from . import db
 from .models import Book
 
@@ -7,84 +6,84 @@ from .models import Book
 # Get books with query string from api
 def fetch_books(query_string):
 
-	url = 'https://www.googleapis.com/books/v1/volumes?q=' + str(query_string)
-	res = requests.get(url)
-	res_json = res.json()
+    url = 'https://www.googleapis.com/books/v1/volumes?q=' + str(query_string)
+    res = requests.get(url)
+    res_json = res.json()
 
-	if res.status_code != 200 or res_json['totalItems'] < 1:
-		return None
+    if res.status_code != 200 or res_json['totalItems'] < 1:
+        return None
 
-	return res_json['items']
+    return res_json['items']
 
 
 # Get book info by id form api
 def fetch_info(book_id):
 
-	url = "https://www.googleapis.com/books/v1/volumes/" + book_id
-	res = requests.get(url)
-	return res.json() if res.status_code == 200 else None
+    url = "https://www.googleapis.com/books/v1/volumes/" + book_id
+    res = requests.get(url)
+    return res.json() if res.status_code == 200 else None
 
 
 # Add book to database table if doesn't exist already
 def add_book_to_table(book_info):
 
-	book_id = book_info['id']
+    book_id = book_info['id']
 
-	# If book already exists return id
-	book = Book.query.filter_by(book_id=book_id).first()
-	if book:
-		return
-	
-	# Create new book instance
-	new_book = create_book_instance(book_info)
+    # If book already exists return id
+    book = Book.query.filter_by(book_id=book_id).first()
+    if book:
+        return
 
-	# Add book to sessions
-	db.session.add(new_book)
+    # Create new book instance
+    new_book = create_book_instance(book_info)
+
+    # Add book to sessions
+    db.session.add(new_book)
 
 
 def remove_book_from_table(book_id):
 
-	# Delete if book with id exists
-	book = Book.query.filter_by(book_id=book_id).first()
-	if book:
-		db.session.delete(book)
+    # Delete if book with id exists
+    book = Book.query.filter_by(book_id=book_id).first()
+    if book:
+        db.session.delete(book)
 
 
 def create_book_instance(book_info):
 
-	new_book = Book(book_id = book_info['id'])
+    new_book = Book(book_id=book_info['id'])
 
-	try:
-		new_book.title = book_info['volumeInfo']['title']
-	except:
-		new_book.title = 'No title'
-	try:
-		new_book.authors = ", ".join(book_info['volumeInfo']['authors'])
-	except:
-		new_book.authors = 'Unknown'
-	try:
-		new_book.published_date = str(book_info['volumeInfo']['publishedDate'])
-	except:
-		new_book.published_date = 'Unknown'
-	try:
-		new_book.description = book_info['volumeInfo']['description']
-	except:
-		new_book.description = 'No description provided.'
-	try:
-		new_book.categories = ", ".join(book_info['volumeInfo']['categories'])
-	except:
-		new_book.categories = 'Unknown'
-	try:
-		new_book.small_url = book_info['volumeInfo']['imageLinks']['thumbnail']
-	except:
-		new_book.small_url = '/images/no-cover.jpg'
-	try:
-		new_book.large_url = book_info['volumeInfo']['imageLinks']['small']
-	except:
-		new_book.large_url = new_book.small_url
-	try:
-		new_book.preview_link = book_info['volumeInfo']['previewLink']
-	except:
-		new_book.preview_link = '#'
+    try:
+        new_book.title = book_info['volumeInfo']['title']
+    except:
+        new_book.title = 'No title'
+    try:
+        new_book.authors = ", ".join(book_info['volumeInfo']['authors'])
+    except:
+        new_book.authors = 'Unknown'
+    try:
+        new_book.published_date = str(book_info['volumeInfo']['publishedDate'])
+    except:
+        new_book.published_date = 'Unknown'
+    try:
+        new_book.description = book_info['volumeInfo']['description']
+    except:
+        new_book.description = 'No description provided.'
+    try:
+        new_book.categories = ", ".join(book_info['volumeInfo']['categories'])
+    except:
+        new_book.categories = 'Unknown'
+    try:
+        new_book.small_url = book_info['volumeInfo']['imageLinks']['thumbnail']
+    except:
+        new_book.small_url = '/images/no-cover.jpg'
+    try:
+        new_book.large_url = book_info['volumeInfo']['imageLinks']['small']
+    except:
+        new_book.large_url = new_book.small_url
+    try:
+        new_book.preview_link = book_info['volumeInfo']['previewLink']
+    except:
+        new_book.preview_link = '#'
 
-	return new_book
+    return new_book
